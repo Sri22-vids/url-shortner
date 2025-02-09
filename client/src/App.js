7e8fe4237c67f21a9bc7211575f27e516ee84f34
Srivats Venkataraman <42980667+srivats22@users.noreply.github.com>
2025-02-09 14:59:32 -0500

    EP2 push

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,9 @@ import axios from "axios";
 
 export default function App() {
   // Step 1: Create State Variables
-  const [longUrl, setLongUrl] = React.useState("")
-  const [allUrls, setAllUrls] = React.useState([])
   
   // Step 2: Complete handleFormSubmit function
-  const handleFormSubmit = (e) => {
-    e.preventDefault()
-    if(longUrl === "" || longUrl.trim() === ""){
-      window.alert("Long URL is required")
-    }
-    else{
-      var reqBody = {
-        longUrl: longUrl,
-      }
-      axios.post(
-        'http://localhost:4000/shorten-url', reqBody
-      )
-      .then((resp) => {
-        setAllUrls([...allUrls, resp.data])
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    }
-  };
+  const handleFormSubmit = (e) => {};
 
   // Step 3: Complete return statement
   return (
@@ -44,8 +23,8 @@ export default function App() {
           <nav className="no-space">
             <div className="max field border left-round">
               <input
-                value={longUrl}
-                onChange={(e) => setLongUrl(e.target.value)}
+                value={{}}
+                onChange={(e) => {}}
                 placeholder="Enter Long URL"
                 onKeyDown={(e) => {
                   if(e.key === "Enter"){
@@ -56,7 +35,7 @@ export default function App() {
             </div>
             <button
               className="large right-round"
-              onClick={(e) => {handleFormSubmit(e)}}
+              onClick={(e) => {}}
             >
               Submit
             </button>
@@ -64,11 +43,6 @@ export default function App() {
         </div>
       </article>
       {/* Step 4: Display all URLS */}
-      {
-        allUrls.map((url) => (
-          <UrlCard urlObj={url}/>
-        ))
-      }
     </>
   );
 }
